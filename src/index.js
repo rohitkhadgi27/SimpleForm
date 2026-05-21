403a47f552f98b638fc2dc3170bfa45ec9507cc0
Rohit <rohitkhadgi27@gmail.com>
2026-05-22 00:21:15 +1000

    preparation for deployment

--- src/index.js
+++ src/index.js
@@ -38,11 +38,14 @@ app.use(passport.session());
 
 // Database connection pool setup
 const db = new Pool({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === "production" ? {rejectUnauthorized: false} : false,
+  // user: process.env.PG_USER,
+  // host: process.env.PG_HOST,
+  // database: process.env.PG_DATABASE,
+  // password: process.env.PG_PASSWORD,
+  // port: process.env.PG_PORT,
+
 });
 
 // Passport Local Strategy for authentication
