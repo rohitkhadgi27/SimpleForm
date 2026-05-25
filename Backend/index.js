16d0f8e7cf4d0fddfa80e2bf61d8de94f89e19a2
Rohit <rohitkhadgi27@gmail.com>
2026-05-26 00:30:48 +1000

    final

--- Backend/index.js
+++ Backend/index.js
@@ -89,42 +89,34 @@ passport.use("google", new GoogleStrategy({
 }));
 
 // putting data in the session
-// passport.serializeUser((user, cb) => {
-//   cb(null, user.email);   // store only email
-// });
 passport.serializeUser((user, cb) => {
-  return cb(null, user);
+  cb(null, user.email);   // store only email
 });
+// passport.serializeUser((user, cb) => {
+//   return cb(null, user);
+// });
 
 // retrieving the user data from the session
-// passport.deserializeUser(async (email, cb) => {
-//   try {
-//     const result = await db.query("SELECT * FROM users WHERE email = $1", [email]);
-//     cb(null, result.rows[0]);
-//   } catch (err) {
-//     cb(err);
-//   }
-// });
-passport.deserializeUser((user, cb) => {
-  return cb(null, user);
+passport.deserializeUser(async (email, cb) => {
+  try {
+    const result = await db.query("SELECT * FROM users WHERE email = $1", [email]);
+    cb(null, result.rows[0]);
+  } catch (err) {
+    cb(err);
+  }
 });
+// passport.deserializeUser((user, cb) => {
+//   return cb(null, user);
+// });
 
 //********************GET ROUTES ************************************************************ */
 app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
-// app.get('/auth/google/secrets', passport.authenticate('google',
-//   {
-//     successRedirect: process.env.FRONTEND_URL + '/userPortal',
-//     failureRedirect: process.env.FRONTEND_URL
+app.get('/auth/google/secrets', passport.authenticate('google',
+  {
+    successRedirect: process.env.FRONTEND_URL + '/userPortal',
+    failureRedirect: process.env.FRONTEND_URL
 
-//   }));
-app.get(
-  "/auth/google/secrets",
-  passport.authenticate("google", { failureRedirect: process.env.FRONTEND_URL }),
-  (req, res) => {
-    res.redirect(process.env.FRONTEND_URL + "/userPortal");
-  }
-);
-
+  }));
 
 // Getting the user info from the database and sending it to the frontend
 app.get('/userInfo', async (req, res) => {
